24685818c9377fefce38c216d782d5a7bc5c9f31
veschenbrenner <veschenbrenner@etu.unistra.fr>
2023-06-06 10:01:35 +0200

    Les points d'intersection sont mis dans une liste pour chaque duo de courbe

--- main.py
+++ main.py
@@ -21,17 +21,22 @@ while continuer==True:
         continuer = False
     
 
+for i in range(1,nb_fichier-1):
+    for j in range(i+1, nb_fichier):
+        exec('point_intersection_'+str(i)+'_et_'+str(j)+'= [[0],[0]]')
 
-point_intersection = [[0],[0]]
-courbe_inf_int = []
+for i in range(1,nb_fichier-1):
+    for j in range(i+1, nb_fichier):
+        exec('courbe_inf_int_'+str(i)+'_et_'+str(j)+'= []')
 
-def comparaison_point(y1,y2,x1,x2):
+
+def comparaison_point(y1,y2,x1,x2,num_courbe1, num_courbe2):
     if y1[0] < y2[0]:
         courbe_inf = 1
-        courbe_inf_int.append(1)
+        exec('courbe_inf_int_'+str(num_courbe1)+'_et_'+str(num_courbe2)+'.append(1)')
     elif y1[0]> y2[0]:
         courbe_inf = 2
-        courbe_inf_int.append(2)
+        exec('courbe_inf_int_'+str(num_courbe1)+'_et_'+str(num_courbe2)+'.append(2)')
     else:
         courbe_inf=0
         
@@ -39,28 +44,28 @@ def comparaison_point(y1,y2,x1,x2):
     for i in range(len(y1)-2):
         if y1[i+1] < y2[i+1]:
             if courbe_inf == 2:
-                intersection(y1[i], y2[i], y1[i+1], y2[i+1],x1[i],x1[i+1])
+                intersection(y1[i], y2[i], y1[i+1], y2[i+1],x1[i],x1[i+1],num_courbe1,num_courbe2)
                 courbe_inf = 1
-                courbe_inf_int.append(1)
+                exec('courbe_inf_int_'+str(num_courbe1)+'_et_'+str(num_courbe2)+'.append(1)')
             if courbe_inf == 0:
                 courbe_inf = 1
-                courbe_inf_int.append(1)
+                exec('courbe_inf_int_'+str(num_courbe1)+'_et_'+str(num_courbe2)+'.append(1)')
         elif y1[i+1] == y2[i+1]:
-            point_intersection[0].append(i+1)
-            point_intersection[1].append(y1[i+1])
+            exec('point_intersection_'+str(num_courbe1)+'_et_'+str(num_courbe2)+'[0].append(i+1)')
+            exec('point_intersection_'+str(num_courbe1)+'_et_'+str(num_courbe2)+'[1].append(y1[i+1])')
             courbe_inf == 0
         else : 
             if courbe_inf == 1:
-                intersection(y1[i], y2[i], y1[i+1], y2[i+1],x1[i],x1[i+1])
-                courbe_inf_int.append(2)
+                intersection(y1[i], y2[i], y1[i+1], y2[i+1],x1[i],x1[i+1],num_courbe1,num_courbe2)
+                exec('courbe_inf_int_'+str(num_courbe1)+'_et_'+str(num_courbe2)+'.append(2)')
                 courbe_inf = 2
             if courbe_inf == 0:
                 courbe_inf = 2
-                courbe_inf_int.append(2)
+                exec('courbe_inf_int_'+str(num_courbe1)+'_et_'+str(num_courbe2)+'.append(2)')
     
 
 
-def intersection(y1n, y2n, y1n1, y2n1,x,xn1):
+def intersection(y1n, y2n, y1n1, y2n1,x,xn1, num_courbe1,num_courbe2):
     #Recherche des fonction affines
     a1 = (y1n1-y1n)/(xn1-x)
     a2 = (y2n1-y2n)/(xn1-x)
@@ -69,22 +74,25 @@ def intersection(y1n, y2n, y1n1, y2n1,x,xn1):
     #Recherche du point d'intersection
     x_int = (b2-b1)/(a1-a2)
     y_int = (a1*b2-b1*a2)/(a1-a2)
-    point_intersection[0].append(x_int)
-    point_intersection[1].append(y_int)
+    exec('point_intersection_'+str(num_courbe1)+'_et_'+str(num_courbe2)+'[0].append(x_int)')
+    exec('point_intersection_'+str(num_courbe1)+'_et_'+str(num_courbe2)+'[1].append(y_int)')
 
 
 for i in range(1,nb_fichier-1):
     for j in range(i+1,nb_fichier):
-        exec('comparaison_point(y'+str(i)+',y'+str(j)+',x'+str(i)+',x'+str(j)+')')
+        exec('comparaison_point(y'+str(i)+',y'+str(j)+',x'+str(i)+',x'+str(j)+','+str(i)+','+str(j)+')')
 
-with open("point_d'intersection.dat","w")as fichier:
-    for i in range(len(point_intersection[0])-1):
-        fichier.write(str(point_intersection[0][i])+' '+str(point_intersection[1][i]))
+# with open("point_d'intersection.dat","w")as fichier:
+#     for i in range(len(point_intersection[0])-1):
+#         fichier.write(str(point_intersection[0][i])+' '+str(point_intersection[1][i]))
 
 for i in range(1,nb_fichier):
     exec('plt.plot(x'+ str(i)+',y'+str(i)+')')
 
+for i in range(1,nb_fichier-1):
+    for j in range(i+1,nb_fichier):
+        exec("plt.plot(point_intersection_"+str(i)+"_et_"+str(j)+"[0],point_intersection_"+str(i)+"_et_"+str(j)+"[1], 'ro')")
 
-plt.plot(point_intersection[0],point_intersection[1], 'ro')
+
 
 plt.show()
